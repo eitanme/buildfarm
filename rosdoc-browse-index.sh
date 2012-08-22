3943517908e68c46320c4f52b86db217fcc99282
Tully Foote <tfoote@willowgarage.com>
2012-08-22 10:44:14 -0700

    no more fuerte specific indexer usage.

--- rosdoc-browse-index.sh
+++ rosdoc-browse-index.sh
@@ -15,7 +15,7 @@ export ROS_LANG_DISABLE=roseus:rosoct:rosjava
 sudo apt-get install ros-fuerte-documentation -y
 
 # create repos.rosinstall
-wget -O /tmp/generate_index.py https://code.ros.org/svn/ros/stacks/rosorg/trunk/rosdoc_rosorg/scripts/generate_fuerte_index.py
+wget -O /tmp/generate_index.py https://code.ros.org/svn/ros/stacks/rosorg/trunk/rosdoc_rosorg/scripts/generate_index.py
 chmod +x /tmp/generate_index.py
 /tmp/generate_index.py /tmp/repos.rosinstall --rosdistro fuerte
 cat /tmp/repos.rosinstall
