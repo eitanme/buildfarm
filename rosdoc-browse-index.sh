e5bdb0979546d2d941fade60af81062c994da980
Tully Foote <tfoote@willowgarage.com>
2012-08-22 15:36:50 -0700

    Update rosdoc-browse-index.sh
    There's no need to build this..... grrr

--- rosdoc-browse-index.sh
+++ rosdoc-browse-index.sh
@@ -38,7 +38,5 @@ cat /etc/apt/sources.list.d/ros-latest.list
 rosdep update
 rosdep install rosdoc_rosorg -y
 
-rosmake rosdoc_rosorg --status-rate=0
-
 echo "running rosdoc_rosorg on index"
 cd `rospack find rosdoc_rosorg` && rosrun rosdoc_rosorg rosdoc_rosorg.py -o /tmp/doc --upload=wgs32:/var/www/www.ros.org/html/browse --checkout=$WORKSPACE/rosdoc_checkout --rosbrowse  --repos /tmp/repos.rosinstall
