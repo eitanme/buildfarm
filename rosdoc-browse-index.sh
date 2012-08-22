e5009d8d25f6f6329229851a088f23262b4657f8
Tully Foote <tfoote@willowgarage.com>
2012-08-21 21:41:00 -0700

    switch to fuerte

--- rosdoc-browse-index.sh
+++ rosdoc-browse-index.sh
@@ -11,13 +11,13 @@ sudo easy_install -U vcstools rospkg rosinstall sphinx
 
 export ROS_LANG_DISABLE=roseus:rosoct:rosjava
 
-# use ros-electric for indexer
-sudo apt-get install ros-electric-documentation -y
+# use ros-fuerte for indexer
+sudo apt-get install ros-fuerte-documentation -y
 
 # create repos.rosinstall
-wget -O /tmp/generate_index.py https://code.ros.org/svn/ros/stacks/rosorg/trunk/rosdoc_rosorg/scripts/generate_index.py
+wget -O /tmp/generate_index.py https://code.ros.org/svn/ros/stacks/rosorg/trunk/rosdoc_rosorg/scripts/generate_fuerte_index.py
 chmod +x /tmp/generate_index.py
-/tmp/generate_index.py /tmp/repos.rosinstall
+/tmp/generate_index.py /tmp/repos.rosinstall --rosdistro fuerte
 cat /tmp/repos.rosinstall
 
 rosinstall -j8 $WORKSPACE/rosdoc_checkout /tmp/repos.rosinstall -n --continue-on-error --delete-changed-uris
