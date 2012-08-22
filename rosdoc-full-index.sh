3b011c75dd131a3b0a015fc1df29d138ee28c8af
Tully Foote <tfoote@willowgarage.com>
2012-08-22 10:42:33 -0700

    pip to apt installation
    for rospkg, rosdep, rosinstall, vcstools

--- rosdoc-full-index.sh
+++ rosdoc-full-index.sh
@@ -8,8 +8,8 @@ sudo apt-get install -y python-software-properties
 sudo add-apt-repository ppa:bzr/ppa
 sudo apt-get update
 
-sudo apt-get install python-setuptools mercurial bzr git-core doxygen python-epydoc python-svn pkg-config -y 
-sudo easy_install -U vcstools rospkg rosinstall rosdep sphinx
+sudo apt-get install python-setuptools mercurial bzr git-core doxygen python-epydoc python-svn pkg-config python-vcstools python-rospkg python-rosinstall python-rosdep -y 
+sudo easy_install -U sphinx
 
 export ROS_LANG_DISABLE=roseus:rosoct:rosjava
 
