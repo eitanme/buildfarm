504a9f22f53da2bb5892866ac4decb67da843389
Tully Foote <tfoote@willowgarage.com>
2012-08-23 10:29:46 -0700

    speedups for debugging

--- rosdoc-browse-index.sh
+++ rosdoc-browse-index.sh
@@ -17,12 +17,12 @@ sudo apt-get install ros-fuerte-documentation -y
 # create repos.rosinstall
 wget -O /tmp/generate_index.py https://code.ros.org/svn/ros/stacks/rosorg/trunk/rosdoc_rosorg/scripts/generate_index.py
 chmod +x /tmp/generate_index.py
-/tmp/generate_index.py /tmp/repos.rosinstall --rosdistro fuerte
+#/tmp/generate_index.py /tmp/repos.rosinstall --rosdistro fuerte
 cat /tmp/repos.rosinstall
 
 #clear the rosinstall of old files
-rm -f $WORKSPACE/rosdoc_checkout/.rosinstall
-rosinstall -j8 $WORKSPACE/rosdoc_checkout /tmp/repos.rosinstall -n --continue-on-error --delete-changed-uris || true
+#rm -f $WORKSPACE/rosdoc_checkout/.rosinstall
+#rosinstall -j8 $WORKSPACE/rosdoc_checkout /tmp/repos.rosinstall -n --continue-on-error --delete-changed-uris || true
 
 cat $WORKSPACE/rosdoc_checkout/setup.sh
 . $WORKSPACE/rosdoc_checkout/setup.sh
