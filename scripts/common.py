df4f26e29f0619f1fc5170a7396856a0fa5c6fce
Wim Meeussen <wim@wim.gateway.2wire.net>
2012-10-15 17:15:03 -0700

    starting to build dependency database

--- scripts/common.py
+++ scripts/common.py
@@ -4,13 +4,51 @@ import subprocess
 import sys
 import fnmatch
 import yaml
+import threading
 import datetime
+from Queue import Queue
+from threading import Thread
 from xml.etree.ElementTree import ElementTree
 
 def append_pymodules_if_needed():
     #TODO: This is a hack, in the chroot, the default python path does not
     if not os.path.abspath("/usr/lib/pymodules/python2.7") in sys.path:
         sys.path.append("/usr/lib/pymodules/python2.7")
+
+
+
+
+## {{{ http://code.activestate.com/recipes/577187/ (r9)
+class Worker(Thread):
+    """Thread executing tasks from a given tasks queue"""
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+    
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try: func(*args, **kargs)
+            except Exception, e: print e
+            self.tasks.task_done()
+
+class ThreadPool:
+    """Pool of threads consuming tasks from a queue"""
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads): Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        """Add a task to the queue"""
+        self.tasks.put((func, args, kargs))
+
+    def wait_completion(self):
+        """Wait for completion of all the tasks in the queue"""
+        self.tasks.join()
+
+
 
 class DevelDistro:
     def __init__(self, name):
@@ -26,20 +64,26 @@ class DevelDistroRepo:
         self.name = name
         self.url = data['url']
         self.type = data['type']
-        self.version = 'void'
+        self.version = None
         if 'version' in data.keys():
             self.version = data['version']
             
     def get_rosinstall(self):
-        return yaml.dump([{self.type: {'local-name': self.name, 'uri': '%s'%self.url, 'version': '%s'%self.version}}], default_style=False)
+        if self.version:
+            return yaml.dump([{self.type: {'local-name': self.name, 'uri': '%s'%self.url, 'version': '%s'%self.version}}], default_style=False)
+        else:
+            return yaml.dump([{self.type: {'local-name': self.name, 'uri': '%s'%self.url}}], default_style=False)
+
+
 
 
 class RosDistro:
-    def __init__(self, name):
+    def __init__(self, name, initialize_dependencies=False):
         url = urllib.urlopen('https://raw.github.com/ros/rosdistro/master/releases/%s.yaml'%name)
         distro = yaml.load(url.read())['repositories']
         self.repositories = {}
         self.packages = {}
+        self.threadpool = ThreadPool(5)
         for repo_name, data in distro.iteritems():
             if 'packages' in data.keys():
                 pkgs = []
@@ -47,6 +91,8 @@ class RosDistro:
                 version = data['version']
                 for pkg_name in data['packages'].keys():
                     pkg = RosDistroPackage(pkg_name, url, version)
+                    if initialize_dependencies:
+                        self.threadpool.add_task(pkg.initialize_dependencies)
                     pkgs.append(pkg)
                     self.packages[pkg_name] = pkg
                 self.repositories[repo_name] = RosDistroRepo(repo_name, pkgs)
@@ -70,11 +116,32 @@ class RosDistroRepo:
         return rosinstall
 
 
+
 class RosDistroPackage:
-    def __init__(self, name, url, version):
+    def __init__(self, name, url, version, initialize_dependencies=False):
+        self.lock = threading.Lock()
+        self.initialized = False
         self.name = name
         self.url = url
         self.version = version.split('-')[0]
+        
+    def initialize_dependencies(self):
+        with self.lock:
+            http = self.url
+            http = http.replace('.git', '/release')
+            http = http.replace('git://', 'http://raw.')
+            url = '%s/%s/%s/package.xml'%(http, self.name, self.version)
+            package_xml = urllib.urlopen(url).read()
+            append_pymodules_if_needed()
+            from catkin_pkg import package
+            try:
+                pkg = package.parse_package_string(package_xml)
+            except package.InvalidPackage as e:
+                print "!!!!!! Invalid package.xml for package %s at url %s"%(self.name, url)
+                raise BuildException("Invalid package.xml")
+            self.build_depends_on1 = [d.name for d in pkg.build_depends]
+            self.test_depends_on1 = [d.name for d in pkg.test_depends]
+            self.initialized = True
 
     def get_rosinstall_release(self, version=None):
         if not version:
@@ -130,6 +197,8 @@ class AptDepends:
                 if not one:
                     self.depends_on(p, res, one)
         return res
+
+
 
 class RosDepResolver:
     def __init__(self, ros_distro):
@@ -240,7 +309,7 @@ def get_ros_env(setup_file):
     return ros_env
 
 def call_with_list(command, envir=None):
-    print "Executing command '%s'"%command
+    print "Executing command '%s'"%' '.join(command)
     helper = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True, env=envir)
     res, err = helper.communicate()
     print str(res)
@@ -324,7 +393,9 @@ def get_dependency_build_order(depends):
 
     return order
 
-def get_dependencies(source_folder):
+
+
+def get_dependencies(source_folder, build_depends=True, test_depends=True):
     # get the dependencies
     print "Get the dependencies of source folder %s"%source_folder
     append_pymodules_if_needed()
@@ -338,9 +409,15 @@ def get_dependencies(source_folder):
 
     depends = []
     for name, pkg in pkgs.iteritems():
-        for d in pkg.build_depends + pkg.test_depends:
-            if not d.name in depends and not d.name in local_packages:
-                depends.append(d.name)
+        if build_depends:
+            for d in pkg.build_depends:
+                if not d.name in depends and not d.name in local_packages:
+                    depends.append(d.name)
+        if test_depends:
+            for d in pkg.test_depends:
+                if not d.name in depends and not d.name in local_packages:
+                    depends.append(d.name)
+
     return depends
 
 
